99a28f2ea52c14d366549022baedfb282e61eace
Rafael <rafaelefucho@gmail.com>
2019-02-06 14:28:50 +0100

    Fourier with DFT function

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
@@ -13,18 +14,51 @@ public class Main extends JComponent implements Runnable {
     static int WidthPanel = 360 * 2;
     double time = Math.PI;
     ArrayList<Point2D> wave;
+    ArrayList<ResultImaginary> fourierY;
+
 
     public Main() {
         wave = new ArrayList<>();
+        wave = new ArrayList<>();
+
+        ArrayList<Double> yImage = new ArrayList<>();
+
+        fourierY = dft(yImage);
+
         Thread t = new Thread(this);
         t.start();
+    }
+
+    private ArrayList<ResultImaginary> dft(ArrayList<Double> yImage) {
+        ArrayList<ResultImaginary> result = new ArrayList<>();
+        int N = yImage.size();
+
+        for (int k = 0; k < N; k++) {
+            double re = 0;
+            double im = 0;
+            for (int n = 0; n < N; n++) {
+                double phi = (2 * Math.PI * k * n) / N;
+                re += yImage.get(n) * Math.cos(phi);
+                im -= yImage.get(n) * Math.sin(phi);
+            }
+
+
+            re = re / N;
+            im = im / N;
+            
+            result.add(k, new ResultImaginary(re,im,k));
+
+        }
+
+
+        return result;
     }
 
     private void timeStep() {
         time += 0.05;
 
-        if (wave.size() > 500){
-            wave.remove(wave.size()-1);
+        if (wave.size() > 500) {
+            wave.remove(wave.size() - 1);
         }
     }
 
@@ -38,25 +72,36 @@ public class Main extends JComponent implements Runnable {
         double y = 0;
         double radius;
 
-        for (int i=0;i<20;i+= 2){
-            radius = 100/(i+1);
-            drawCenteredCircle(g2,x,y,radius);
+        for (int i = 0; i < 30; i += 2) {
 
-            x = radius/2 * (Math.cos(time*(i+1)) + 0) + x;
-            y = radius/2 * (Math.sin(time*(i+1)) + 0) + y;
+            radius = 150 / (i + 1);
+
+            g2.setStroke(new BasicStroke(1 / 2));
+            drawCenteredCircle(g2, x, y, radius);
+            double xC = x;
+            double yC = y;
+
+
+            x = radius / 2 * (Math.cos(time * (i + 1)) + 0) + x;
+            y = radius / 2 * (Math.sin(time * (i + 1)) + 0) + y;
+
+            g2.setStroke(new BasicStroke(1));
+            Line2D line2D = new Line2D.Double(xC, yC, x, y);
+            g2.draw(line2D);
+
 
         }
 
-        wave.add(0,new Point2D.Double(x, y));
+        wave.add(0, new Point2D.Double(x, y));
 
 
-        drawPoint(g2,x,y);
+        drawPoint(g2, x, y);
 
         Path2D path2D = new Path2D.Float();
-        path2D.moveTo(200, wave.get(0).getY());
-        int index=0;
+        path2D.moveTo(250, wave.get(0).getY());
+        int index = 0;
         for (Point2D temp : wave) {
-            path2D.lineTo(index+200,temp.getY());
+            path2D.lineTo(index + 250, temp.getY());
             index++;
         }
 
@@ -67,17 +112,17 @@ public class Main extends JComponent implements Runnable {
 
     private void drawPoint(Graphics2D g2, double x, double y) {
         double r = 4;
-        x = x-(r/2);
-        y = y-(r/2);
-        Ellipse2D ellipse2D = new Ellipse2D.Double(x, y, r,r);
+        x = x - (r / 2);
+        y = y - (r / 2);
+        Ellipse2D ellipse2D = new Ellipse2D.Double(x, y, r, r);
         g2.fill(ellipse2D);
 
     }
 
     public void drawCenteredCircle(Graphics2D g, double x, double y, double r) {
-        x = x-(r/2);
-        y = y-(r/2);
-        Ellipse2D ellipse2D = new Ellipse2D.Double(x, y, r,r);
+        x = x - (r / 2);
+        y = y - (r / 2);
+        Ellipse2D ellipse2D = new Ellipse2D.Double(x, y, r, r);
         g.draw(ellipse2D);
     }
 
